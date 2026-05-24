171c15497e9cfce2ccf86d650f500ba3bd26ca32
Huzaifa Qureshi <198439864+muhammad-huzaifa-qureshi@users.noreply.github.com>
2026-05-24 23:56:36 +0500

    requirements file added, central config done in backend

---
+++ backend/config.py
@@ -0,0 +1,11 @@
+"""
+Centralised all environment variable access
+"""
+from dotenv import load_dotenv
+
+import os
+load_dotenv()
+GROQ_API_KEY = os.getenv('GROQ_API_KEY')
+TAVILY_API_KEY = os.getenv('TAVILY_API_KEY')
+MONGODB_URI = os.getenv('MONGODB_URI')
+CORS_ORIGIN = os.getenv('CORS_ORIGIN')
